eb36c9e8177627c7e40fa88291f03b781dc5a6b9
abdullah-mansoor3 <mabm322005@gmail.com>
2025-04-09 16:46:57 +0500

    fix: Load env variables

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,14 @@
+require('dotenv').config();  // Load environment variables from .env
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const config = require('config');
 
-require('./startup/db');            // connect to MySQL
-require('./startup/routes')(app); // setup routes
+require('./startup/db');            // Connect to MySQL
+require('./startup/routes')(app);   // Set up routes
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+    console.log(`Server running on port ${port}`);
 });
 
 module.exports = app;
